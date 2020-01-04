1036cd94664484342f3ca4600b01b1b6b484cc8a
vasilijus <vasiliokas@gmail.com>
2020-01-04 13:40:01 +0000

    path

--- JS - Practice 01-SVG/sub.js
+++ JS - Practice 01-SVG/sub.js
@@ -8,23 +8,24 @@ const circle1 = createCircle({
     minDuration: 5,
     maxDuration: 20
 })
+const path1 = createPath({
+    cx: 10, cy: 10
+})
 
+function createPath() {
+
+}
 
 function getRandomInt(min, max) {
   return Math.random() *(max - min + 1) + min;
 }
 
-function get2Points( ) {
-
-}
-
-function makeCurbe() {
-
-}
 
 function createCircle(options) {
     // setInterval(function() {
       options.element.setAttribute('r', getRandomInt(options.minRadius, options.maxRadius));
+      options.element.setAttribute('cx', 50);
+      options.element.setAttribute('cy', 120);
     // }, 1000);
 }
 
