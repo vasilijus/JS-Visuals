0f82972355df0aa6e868a11b2afd4d0606120005
vasilijus <vasiliokas@gmail.com>
2020-01-04 14:39:46 +0000

    SVG curve, Point1 to Point2

---
+++ JS - Practice 01-SVG/Backup/main copy.js
@@ -0,0 +1,125 @@
+const hoverItem = document.querySelector('p')
+var ct = 0;
+
+const blob1 = createBlob({
+    element: document.querySelector('#blob'),
+    numPoints: 8,
+    centerX: 500,
+    centerY: 500,
+    minRadius: 400,
+    maxRadius: 455,
+    minDuration: 1,
+    maxDuration: 2
+})
+
+hoverItem.addEventListener('mouseenter', () => {
+  TweenMax.to(blob1.tl, 0.3, { 
+    timeScale: 1,
+    onStart() {
+      blob1.tl.play()
+    }
+  });
+});
+
+hoverItem.addEventListener('mouseleave', () => {
+  TweenMax.to(blob1.tl, 2, { 
+    timeScale: 0,
+    onComplete() {
+      blob1.tl.pause()
+    }
+  });
+});
+
+function createBlob(options) {
+    const points = []
+    const path = options.element
+    const slice = (Math.PI * 2) / options.numPoints
+    const startAngle = random(Math.PI * 2)
+
+    const tl = new TimelineMax({
+        onUpdate: update,
+      paused: true
+    })
+
+    for (let i = 0; i < options.numPoints; i++) {
+        const angle = startAngle + i * slice
+        const duration = random(options.minDuration, options.maxDuration)
+
+        const point = {
+            x: options.centerX + Math.cos(angle) * options.minRadius,
+            y: options.centerY + Math.sin(angle) * options.minRadius
+        }
+
+        const tween = TweenMax.to(point, duration, {
+            x: options.centerX + Math.cos(angle) * options.maxRadius,
+            y: options.centerY + Math.sin(angle) * options.maxRadius,
+            repeat: -1,
+            yoyo: true,
+            ease: Sine.easeInOut
+        })
+
+        tl.add(tween, -random(duration))
+        points.push(point)
+    }
+
+    options.tl = tl
+    options.points = points
+  
+  tl.progress(1).progress(0).timeScale(0)
+    update()
+
+    function update() {
+      console.log("UPDATE", ct++)
+        path.setAttribute('d', cardinal(points, true, 1))
+    }
+
+    return options
+}
+
+// Cardinal spline - a uniform Catmull-Rom spline with a tension option
+function cardinal(data, closed, tension) {
+    if (data.length < 1) return 'M0 0'
+    if (tension == null) tension = 1
+
+    let size = data.length - (closed ? 0 : 1)
+    let path = 'M' + data[0].x + ' ' + data[0].y + ' C'
+
+    for (let i = 0; i < size; i++) {
+        let p0, p1, p2, p3
+
+        if (closed) {
+            p0 = data[(i - 1 + size) % size]
+            p1 = data[i]
+            p2 = data[(i + 1) % size]
+            p3 = data[(i + 2) % size]
+        } else {
+            p0 = i == 0 ? data[0] : data[i - 1]
+            p1 = data[i]
+            p2 = data[i + 1]
+            p3 = i == size - 1 ? p2 : data[i + 2]
+        }
+
+        let x1 = p1.x + ((p2.x - p0.x) / 6) * tension
+        let y1 = p1.y + ((p2.y - p0.y) / 6) * tension
+
+        let x2 = p2.x - ((p3.x - p1.x) / 6) * tension
+        let y2 = p2.y - ((p3.y - p1.y) / 6) * tension
+
+        path += ' ' + x1 + ' ' + y1 + ' ' + x2 + ' ' + y2 + ' ' + p2.x + ' ' + p2.y
+    }
+
+    return closed ? path + 'z' : path
+}
+
+function random(min, max) {
+    if (max == null) {
+        max = min
+        min = 0
+    }
+    if (min > max) {
+        var tmp = min
+        min = max
+        max = tmp
+    }
+    return min + (max - min) * Math.random()
+}
--- JS - Practice 01-SVG/sub.js
+++ JS - Practice 01-SVG/sub.js
@@ -1,31 +1,63 @@
+// Window Size
+var w = svgWidth = window.innerWidth;
+var h = svgHeight= window.innerHeight;
 
-const circle1 = createCircle({
-    element: document.querySelector('#path-circle'),
-    centerX: 5,
-    centerY: 5,
-    minRadius: 5,
-    maxRadius: 20,
-    minDuration: 5,
-    maxDuration: 20
+var circle = createCircle(document.querySelector('#circle'), {
+    centerX: 5,    centerY: 5,
+    minRadius: 5, maxRadius: 20,
+    x: 40,         y: 50
 })
-const path1 = createPath({
-    cx: 10, cy: 10
+var point1 = createCircle(document.querySelector('#p1'), {
+    centerX: 5,    centerY: 5,
+    minRadius: 5, maxRadius: 5,
+    x: 0+10,         y: getRandomInt(0,svgHeight)
+})
+var point2 = createCircle(document.querySelector('#p2'), {
+    centerX: 5,    centerY: 5,
+    minRadius: 5, maxRadius: 5,
+    x: svgWidth-10,         y: getRandomInt(0,svgHeight)
 })
 
-function createPath() {
+var p1x = parseFloat(document.getElementById("p1").getAttribute("cx"));
+var p1y = parseFloat(document.getElementById("p1").getAttribute("cy"));
+var p2x = parseFloat(document.getElementById("p2").getAttribute("cx"));
+var p2y = parseFloat(document.getElementById("p2").getAttribute("cy"));
 
+var mpx = (p2x + p1x) * 0.5;
+var mpy = (p2y + p1y) * 0.5;
+// angle of perpendicular to line:
+var theta = Math.atan2(p2y - p1y, p2x - p1x) - Math.PI / 2;
+
+function inOutCurve() {
+  var val = Math.floor(Math.random() * 2)? -50 : 50;
+  return val;
 }
+var offset = inOutCurve();
+
+//location of control point 
+var c1x = mpx + offset * Math.cos(theta);
+var c1y = mpy + offset * Math.sin(theta);
+
+//show control point 
+var cp = document.getElementById('cp');
+cp.setAttribute('cx', c1x);
+cp.setAttribute('cy', c1y);
+
+// draw curve
+var curve = "M" + p1x + " " + p1y + " Q " + c1x + " " + c1y + " " + p2x + " " + p2y;
+var curveEl = document.getElementById('curve')
+curveEl.setAttribute('d', curve);
 
 function getRandomInt(min, max) {
   return Math.random() *(max - min + 1) + min;
 }
 
 
-function createCircle(options) {
+function createCircle(item, options) {
     // setInterval(function() {
-      options.element.setAttribute('r', getRandomInt(options.minRadius, options.maxRadius));
-      options.element.setAttribute('cx', 50);
-      options.element.setAttribute('cy', 120);
+      item.setAttribute('r', getRandomInt(options.minRadius, options.maxRadius));
+      item.setAttribute('cx', options.x);
+      item.setAttribute('cy', options.y);
     // }, 1000);
 }
 
