8f4f5dd13b9d78e0de3e182702f4dbd4da021707
vasilijus <vasiliokas@gmail.com>
2020-02-05 01:09:38 +0000

    circle move

--- JS - Practice 04 - ring pulse/index.js
+++ JS - Practice 04 - ring pulse/index.js
@@ -9,29 +9,43 @@
     }
     init();
 
-    const numOfRings = 1;
-
+    const numOfRings    = 1;
+    let angleStart      = 0;
     function updateRings() {                            // Step 4
         for ( let i = 0; i < numOfRings ; i++ ) {
             drawRing();
         }
+        angleStart++;
+        angleStart >= 360? angleStart = 0 : angleStart++;
     }
 
     const centerX = canvas.width /2;
     const centerY = canvas.height/2;
+    const maxWaveAplitude = 17;
+    const ringRadius = 200;
 
     function drawRing() {                               // Step 5
         ctx.strokeStyle = 'red';
-        ctx.lineWidth = 9;
+        ctx.lineWidth = 6;
         ctx.beginPath();
         ctx.moveTo(centerX,centerY);
-        for( let j = 0; j < 360; j++ ) {
-
-            // setInterval(function(){
-            //     ctx.lineTo(100,300);
-            //     console.log(j);
-            // },1000)
+        for( let j = -180; j < 180; j++ ) {
+            let currentAngle = (j + angleStart) * Math.PI / 180; // (degrees to radians formula)
+            let displacement = 0;
+            let now = Math.abs(j);
             
+            if ( now > 70 ) {
+                displacement = (now-70) / 70;
+            }
+            if ( displacement >= 1 ) {
+                displacement = 1;
+            }
+
+            let waveAmplitude = ringRadius + displacement * Math.sin(currentAngle * 7) * maxWaveAplitude;
+            let x = centerX + Math.cos(currentAngle) * (waveAmplitude);
+            let y = centerY + Math.sin(currentAngle) * (waveAmplitude);
+
+            j > -180 ? ctx.lineTo(x,y) : ctx.moveTo(x,y);
         }
         
         ctx.closePath();
